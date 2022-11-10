10a89cb176bb328f83886aff73cf6389a20c33d5
bijoy karmoakr <bijoykarmokar71@gmail.com>
2022-11-10 14:35:52 +0600

    js upload

--- try.js
+++ try.js
@@ -1,17 +1,14 @@
-(function($){
-    $(document).ready(function() {
-        //console.log('asdfsd');
-        var url = window.location.href;
-        console.log(url);
-        $('body').append('dddddddddddddddddddddddddddddd');
-        //axios.post('https://quickadminpanel.com/builder/103569/preview/files').then(resp => {console.log(resp.data)})
-        //axios.post('http://localhost/quick_admin_crack/download').then(resp => {console.log(resp.data)})
-    });
-    //var url = window.location;
-    return true;
-});
-
-
-/*$.getScript( "http://localhost/try.js", function( data, textStatus, jqxhr ) { 
-    console.log( data );
-})*/
+<script>
+	(function($){
+	    $(document).ready(function() {
+	        //console.log('asdfsd');
+	        //var url = window.location.href;
+	        //console.log(url);
+	        $('body').append('dddddddddddddddddddddddddddddd');
+	        //axios.post('https://quickadminpanel.com/builder/103569/preview/files').then(resp => {console.log(resp.data)})
+	        //axios.post('http://localhost/quick_admin_crack/download').then(resp => {console.log(resp.data)})
+	    });
+	    //var url = window.location;
+	    //return true;
+	});
+</script>
